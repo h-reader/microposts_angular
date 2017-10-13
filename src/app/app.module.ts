762565d33cd36914155b1ff2bf976e1b96039be2
“h-reader” <super.black.jack777@gmail.com>
2017-10-13 23:33:15 +0900

    ServiceLocatorを利用しない方向に転換。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { Injector, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MATERIAL_COMPATIBILITY_MODE, MatInputModule, MatButtonModule, MatIconModule, MatToolbarModule, MatCardModule } from '@angular/material';
+import { MATERIAL_COMPATIBILITY_MODE, MatInputModule, MatButtonModule,
+  MatIconModule, MatToolbarModule, MatCardModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
@@ -13,7 +14,6 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AuthService } from './common/auth/auth.service';
 import { ValidationService } from './common/validation/validation.service';
-import { ServiceLocator } from './common/service-locator';
 
 @NgModule({
   declarations: [
@@ -37,12 +37,10 @@ import { ServiceLocator } from './common/service-locator';
   providers: [
     AuthService,
     ValidationService,
-    { provide: MATERIAL_COMPATIBILITY_MODE, useValue: true },    
+    { provide: MATERIAL_COMPATIBILITY_MODE, useValue: true },
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(private injector: Injector) {
-    ServiceLocator.injector = this.injector;
-  }
+  constructor() {}
  }
