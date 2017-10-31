b213300c9b5799aeb71b62beeb94bb4306e01071
“h-reader” <super.black.jack777@gmail.com>
2017-10-31 23:27:49 +0900

    MessageServiceの呼び出し時、extStrを設定しないとエラーになるバグを修正。

--- src/app/common/message/message.service.ts
+++ src/app/common/message/message.service.ts
@@ -13,14 +13,17 @@ export class MessageService {
    * @param messageKey メッセージキー(MessageKeyクラスのパラメータを利用)
    * @param extStr 置換文字列
    */
-  getMessage(messageKey: string, extStr?: string[]): string {
+  getMessage(messageKey: string, extStr?: any): string {
+
+    var min = extStr && extStr.min ? extStr.min : '';
+    var max = extStr && extStr.max ? extStr.max : '';
 
     var messageList = {
       requied: `値を入力して下さい`,
       email: `正しいメールアドレスを入力してください`,
-      minlength: `${extStr[0]} 文字以上入力してください`,
+      minlength: `${min} 文字以上入力してください`,
+      maxlength: `${max} 文字以内で入力してください`,
       loginError: `メースアドレス、またはパスワードが違います。`,
-
     }
 
     return messageList[messageKey];
